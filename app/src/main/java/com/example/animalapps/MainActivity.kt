f758851536c1db977c60c24b0e592696924ef3b6
Annamarie Cortes <cortes.annamarie@gmail.com>
2024-03-12 04:25:26 -0700

    Added ToolTip Stretch Feature

--- app/src/main/java/com/example/animalapps/MainActivity.kt
+++ app/src/main/java/com/example/animalapps/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.animalapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Tips: FloatingActionButton = findViewById(R.id.Tips)
+        Tips.tooltipText = "Click here for a detailed instruction"
     }
+
 }
